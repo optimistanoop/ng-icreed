379469356a133c1acbacdfaeec153cd389be1d8a
optimistanoop <optimistanoop@gmail.com>
2017-05-13 20:23:06 +0530

    active tab

--- index.js
+++ index.js
@@ -29,21 +29,13 @@ app.controller('icredCtrl', ['$scope','$location',function($scope, $location){
 	
   $scope.tabs = [{name:'Home', url:'#/'},
                  {name:'Home Loan', url:'#tab1'},
-                 {name:'Personal Loan', url:'#tab2'}]
-  
-  
-  
-  
-  
-  
-  
-  
+                 {name:'Personal Loan', url:'#tab2'}];
   
 	$scope.$on('$locationChangeSuccess', function(){
 		var path = $location.path();
-    if(path && path.includes('tab1')) $scope.selection = 'tab1';
-    else if(path && path.includes('tab2')) $scope.selection = 'tab2';
-    else $scope.selection = 'default';
+    if(path && path.includes('tab1')) $scope.selection = '#tab1';
+    else if(path && path.includes('tab2')) $scope.selection = '#tab2';
+    else $scope.selection = '#/';
 	});
 	
 }]);
